c23f386e9e345c94711b5052ced83e2313aafb6a
jposthau <jposthau@LNAR-6BJG7h2.corp.capgemini.com>
2018-08-29 11:29:39 -0500

    "Fixed verification controller errors"

--- src/main/java/com/capgemini/registration/controller/VerificationController.java
+++ src/main/java/com/capgemini/registration/controller/VerificationController.java
@@ -2,36 +2,30 @@ package com.capgemini.registration.controller;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpServlet;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.capgemini.accountopening.service.AccountDetailsService;
-import com.capgemini.accountopening.service.PersonalDetailsService;
 import com.capgemini.registration.model.VerificationDetails;
 
 @Controller
 public class VerificationController {
-	
-	@Autowired
-	private AccountDetailsService accDetailsService;
-	
-	@Autowired 
-	private PersonalDetailsService perDetailsService;
 	
 	@GetMapping("/verification")
 	public String getVerification() {
 		return "verification";
 	}
 	
-	@PostMapping("/verification")
-	public void verify(VerificationDetails verificationDetails) {
-		long accNum = verificationDetails.getAccNum();
-		long custId = accDetailsService.getAccountDetailsById(accNum).getCustomerId();
-		String maiden = perDetailsService.getPersonalDetailsById(custId).getMotherMName();
-		String ssn = perDetailsService.getPersonalDetailsById(custId).getSsn();
-		Date dob = perDetailsService.getPersonalDetailsById(custId).getDob();
-	}
+//	@PostMapping("/verification")
+//	public void verify(VerificationDetails verificationDetails) {
+//		long accNum = verificationDetails.getAccNum();
+//		Date dob = verificationDetails.getDob();
+//		String maiden = verificationDetails.getMaiden();
+//		String ssn = verificationDetails.getSsn();
+//		
+//		String url = "http://localhost:8082/verify/account/"+accNum;
+//	}
 	
 }
